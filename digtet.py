6114b5e7174a42d5a78d6c9841eb140f400ab419
Philip <philiprasmusen@gmail.com>
2020-09-01 10:11:43 +0200

    :)

--- digtet.py
+++ digtet.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-print('Orale')
-if nudes == true be happy :)
-=======
 
 
 print("Døden? Nå dén? Dén optager mig ikke særligt Højst firs procent af tiden Og jeg skærer stadig ned")
->>>>>>> 58f013bf8bba8a0021b8f740128e97e37f770a5a
